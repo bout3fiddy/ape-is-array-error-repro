d9e83651d9b5c5921657d69359ebd8c0736a4180
bout3fiddy <11488427+bout3fiddy@users.noreply.github.com>
2022-07-22 22:10:48 +0200

    more errors

--- contracts/ReturnTypes.vy
+++ contracts/ReturnTypes.vy
@@ -4,3 +4,15 @@
 @view
 def return_uint256_array() -> uint256[20]:
     return empty(uint256[20])
+
+
+@external
+@view
+def return_tuples_uint256_array() -> (uint256[20], uint256[20]):
+    return (empty(uint256[20]), empty(uint256[20]))
+
+
+@external
+@view
+def return_tuples_uint256() -> (uint256, uint256):
+    return (0, 0)
--- tests/test_return_array.py
+++ tests/test_return_array.py
@@ -1,6 +1,15 @@
 import pytest
 
+
 def test_return_array_recursion_error(return_types):
     with pytest.raises(RecursionError):
         return_types.return_uint256_array()
-        
+
+
+def test_return_tuples_uint256_array(return_types):
+    with pytest.raises(RecursionError):
+        assert return_types.return_tuples_uint256_array() == ([0] * 20, [0] * 20)
+
+
+def test_return_tuples_uint256(return_types):
+    assert return_types.return_tuples_uint256() == (0, 0)
